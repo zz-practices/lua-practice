f21c39187ae957b2bb8fbc38971c6bd9aa9a809a
wall-e <18601254750@163.com>
2017-05-04 04:09:54 -0400

    lua interpreter v2

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # lua-practice
 lua practice
+
+## depend
+yum install lua-devel
+yum install readline-devel
+
+## Run
+```
+[root@localhost lua-practice]# ./lua 
+Lua 5.1.4 Copyright (C) 2017 wall-e, zz 
+> t={}
+> t.name="zz"
+> t.age=18
+> print(t)
+table: 0x1e404b0
+> print(t.name)
+zz
+> print(t.age)
+18
+> 
+```
--- lua.c
+++ lua.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include <lua.h>  
 #include <lauxlib.h>  
 #include <lualib.h>  
 
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
 int main (void)
 {
-    char buff[256];
+    char *ptr;
     int error;
     lua_State *L = lua_open();  /* opens Lua */
     luaL_openlibs(L);
+    const char *prompt = "> ";
 
-    while (fgets(buff, sizeof(buff), stdin) != NULL)
-    {
-       error = luaL_loadbuffer(L, buff, strlen(buff), "line") || lua_pcall(L, 0, 0, 0);
-       if (error) 
-       {
-           fprintf(stderr, "%s", lua_tostring(L, -1));
+    fprintf(stdout, "%s\n", LUA_RELEASE " Copyright (C) 2017 wall-e, zz ");
+    while ((ptr = readline(prompt)) != NULL) {
+       error = luaL_loadbuffer(L, ptr, strlen(ptr), "line") || lua_pcall(L, 0, 0, 0);
+       if (error) {
+           fprintf(stderr, "%s\n", lua_tostring(L, -1));
            lua_pop(L, 1);/* pop error message from the stack */
+       } else {
+           add_history(ptr);
        }
+       free(ptr);
     }
 
     lua_close(L);
